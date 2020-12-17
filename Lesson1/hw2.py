21afd9ed4186ef2038d836fc9c3d41050c07db08
Dariya <you@example.com>
2020-12-17 16:01:30 +0200

    hw1

---
+++ Lesson1/hw2.py
@@ -0,0 +1,8 @@
+a = input("Insert a: ")
+b = input("Insert b: ")
+c = input("Insert c: ")
+d = float(b)**2 - 4*float(a)*float(c)
+x1 = (-float(b)+d**0.5)/(2*float(a))
+x2 = (-float(b)-(d**0.5))/(2*float(a))
+print ('x1= ', float(x1), 'x2= ', float(x2));
+
